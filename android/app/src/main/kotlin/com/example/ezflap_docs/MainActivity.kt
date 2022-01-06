77112b0a917a4837914b7936f8a701940f30e8d4
Oz <oz@ezflap.io>
2022-01-06 13:57:30 +0200

    (1) added deployment targets, (2) fixed layout bug in CoreWidgetsEzListTile

---
+++ android/app/src/main/kotlin/com/example/ezflap_docs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ezflap_docs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/Examples/DeepDive/CoreWidgets/CoreWidgetsEzListTile/CoreWidgetsEzListTile.dart
+++ lib/Examples/DeepDive/CoreWidgets/CoreWidgetsEzListTile/CoreWidgetsEzListTile.dart
@@ -11,41 +11,41 @@ class CoreWidgetsEzListTile extends EzStatefulWidgetBase {
 	CoreWidgetsEzListTileState createState() => CoreWidgetsEzListTileState();
 }
 
-	@EzWidget()
-	class CoreWidgetsEzListTileState extends _EzStateBase {
-		/* ZML (8) **/
-		static const _ZML = """
-			<Column>
-				<ListView>
-					<EzListTile z-on:tap="onTapped1">
-						<leading-><Icon z-bind:0="Icons.arrow_circle_down" /></leading->
-						<title-><Text>Item 1. Taps: {{ counter1 }}</Text></title->
-					</EzListTile>
-					<EzListTile z-on:tap="onTapped2">
-						<leading-><Icon z-bind:0="Icons.arrow_circle_up" /></leading->
-						<title-><Text>Item 2. Taps: {{ counter2 }}</Text></title->
-					</EzListTile>
-				</ListView>
-			</Column>
-		""";
+@EzWidget()
+class CoreWidgetsEzListTileState extends _EzStateBase {
+	/* ZML (8) **/
+	static const _ZML = """
+		<Container z-bind:height="300">
+			<ListView>
+				<EzListTile z-on:tap="onTapped1">
+					<leading-><Icon z-bind:0="Icons.arrow_circle_down" /></leading->
+					<title-><Text>Item 1. Taps: {{ counter1 }}</Text></title->
+				</EzListTile>
+				<EzListTile z-on:tap="onTapped2">
+					<leading-><Icon z-bind:0="Icons.arrow_circle_up" /></leading->
+					<title-><Text>Item 2. Taps: {{ counter2 }}</Text></title->
+				</EzListTile>
+			</ListView>
+		</Container>
+	""";
 
 
-		/* BOUND FIELDS (11) **/
-		@EzField("counter1") int _$counter1 = 0; // --> this._counter1
-		@EzField("counter2") int _$counter2 = 0; // --> this._counter2
+	/* BOUND FIELDS (11) **/
+	@EzField("counter1") int _$counter1 = 0; // --> this._counter1
+	@EzField("counter2") int _$counter2 = 0; // --> this._counter2
 
 
-		/* BOUND METHODS (16) **/
-		@EzMethod("onTapped1")
-		void _boundOnTapped1() {
-		    this._counter1++;
-		}
-
-		@EzMethod("onTapped2")
-		void _boundOnTapped2() {
-		    this._counter2++;
-		}
+	/* BOUND METHODS (16) **/
+	@EzMethod("onTapped1")
+	void _boundOnTapped1() {
+	    this._counter1++;
 	}
+
+	@EzMethod("onTapped2")
+	void _boundOnTapped2() {
+	    this._counter2++;
+	}
+}
 
 
 
